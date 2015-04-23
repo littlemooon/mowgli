f66ef9fa51af81c57714dc5187dcb29f96e6e25b
littlemooon <fredwright0@gmail.com>
2015-04-23 16:53:10 +0200

    expose helpers

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 import Mixin from './src/Mixin';
 import RootMixin from './src/RootMixin';
+import helpers from './src/helpers';
 
 export default {
 	Mixin,
-	RootMixin
+	RootMixin,
+	helpers
 };
